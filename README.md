76876843151b38a3aa04d9230c9facddca1916a5
Yves Wang <zhengjia.wang@nxp.com>
2025-11-09 13:20:27 +0800

    Update readme and format code
    Signed-off-by: Yves Wang <zhengjia.wang@nxp.com>

--- README.md
+++ README.md
@@ -1,12 +1,89 @@
 # cmake_parser
+
 ## Introduction
+
 This is a simple c++ lib which use CMake's own lexical parser to get te AST.
 
 At this point, the cmStandardLexer source file taken from [CMake 3.23.3](https://github.com/Kitware/CMake/releases/tag/v3.29.3).
 The lib is aimed to parse a cmake file and output to a structured format like yaml or json.
 
+## Usage
+
+```shell
+cmparser <file>
+```
+
+Take this repo's CMakeLists.txt as example, it will print following in pipe:
+
+```yaml
+- original_name: cmake_minimum_required
+  line: 4
+  line_end: 4
+  args:
+    - value: VERSION
+      line: 4
+    - value: 3.22.0
+      line: 4
+- original_name: project
+  line: 6
+  line_end: 6
+  args:
+    - value: cmparser
+      line: 6
+- original_name: add_library
+  line: 8
+  line_end: 15
+  args:
+    - value: cmparser
+      line: 8
+    - value: LexerParser/cmListFileLexer.c
+      line: 9
+    - value: LexerParser/cmListFileLexer.in.l
+      line: 10
+    - value: LexerParser/cmStandardLexer.h
+      line: 11
+    - value: LexerParser/EncodingC.c
+      line: 12
+    - value: cmParser.cpp
+      line: 13
+    - value: cmParser.h
+      line: 14
+- original_name: target_include_directories
+  line: 17
+  line_end: 21
+  args:
+    - value: cmparser
+      line: 18
+    - value: PUBLIC
+      line: 19
+    - value: ${CMAKE_CURRENT_BINARY_DIR}
+      line: 20
+    - value: ${CMAKE_CURRENT_SOURCE_DIR}/LexerParser
+      line: 21
+- original_name: add_executable
+  line: 23
+  line_end: 24
+  args:
+    - value: example
+      line: 23
+    - value: main.cpp
+      line: 24
+- original_name: target_link_libraries
+  line: 26
+  line_end: 26
+  args:
+    - value: example
+      line: 26
+    - value: PRIVATE
+      line: 26
+    - value: cmparser
+      line: 26
+```
+
 ## Build
+
 Clone the project and run the following commands:
+
 ```shell
 mkdir build && cd build
 cmake ..
@@ -14,9 +91,9 @@ cmake --build . --target cmparser
 ```
 
 You can also build the example target to try the lib
+
 ```shell
 cmake --build . --target example
 
 example ../CMakeLists.txt
 ```
-Then it will print all functions in yaml format
--- cmParser.h
+++ cmParser.h
@@ -1,107 +1,114 @@
+/*
+ * Copyright (c) 2024 Yves W
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of a project licensed under the BSD 3-Clause License.
+ * See the LICENSE file in the project root for full license text.
+ */
+
 #pragma once
 #ifndef CMPARSER_CMPARSER_H
 #define CMPARSER_CMPARSER_H
 
-#endif //CMPARSER_CMPARSER_H
+#endif // CMPARSER_CMPARSER_H
 
-#include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
+#include <iostream>
 #include <memory>
 #include <sstream>
+#include <string>
+#include <vector>
 
 #include "cmListFileLexer.h"
 
-struct cmListFileArgument
-{
-    enum Delimiter
-    {
-        Unquoted,
-        Quoted,
-        Bracket
-    };
-    cmListFileArgument() = default;
-    cmListFileArgument(std::string v, Delimiter d, long line)
-            : Value(std::move(v))
-            , Delim(d)
-            , Line(line)
-    {
-    }
-    bool operator==(const cmListFileArgument& r) const
-    {
-        return (this->Value == r.Value) && (this->Delim == r.Delim);
-    }
-    bool operator!=(const cmListFileArgument& r) const { return !(*this == r); }
-    std::string Value;
-    Delimiter Delim = Unquoted;
-    long Line = 0;
+struct cmListFileArgument {
+	enum Delimiter {
+		Unquoted,
+		Quoted,
+		Bracket
+	};
+	cmListFileArgument() = default;
+	cmListFileArgument(std::string v, Delimiter d, long line)
+		: Value(std::move(v)), Delim(d), Line(line)
+	{
+	}
+	bool operator==(const cmListFileArgument &r) const
+	{
+		return (this->Value == r.Value) && (this->Delim == r.Delim);
+	}
+	bool operator!=(const cmListFileArgument &r) const
+	{
+		return !(*this == r);
+	}
+	std::string Value;
+	Delimiter Delim = Unquoted;
+	long Line = 0;
 };
 
 class cmListFileFunction
 {
-public:
-    cmListFileFunction(std::string name, long line, long lineEnd,
-                       std::vector<cmListFileArgument> args)
-            : Impl{ std::make_shared<Implementation>(std::move(name), line, lineEnd,
-                                                     std::move(args)) }
-    {
-    }
+      public:
+	cmListFileFunction(std::string name, long line, long lineEnd,
+			   std::vector<cmListFileArgument> args)
+		: Impl{std::make_shared<Implementation>(std::move(name), line, lineEnd,
+							std::move(args))}
+	{
+	}
 
-    std::string const& OriginalName() const noexcept
-    {
-        return this->Impl->OriginalName;
-    }
+	std::string const &OriginalName() const noexcept
+	{
+		return this->Impl->OriginalName;
+	}
 
-    long Line() const noexcept { return this->Impl->Line; }
-    long LineEnd() const noexcept { return this->Impl->LineEnd; }
+	long Line() const noexcept
+	{
+		return this->Impl->Line;
+	}
+	long LineEnd() const noexcept
+	{
+		return this->Impl->LineEnd;
+	}
 
-    std::vector<cmListFileArgument> const& Arguments() const noexcept
-    {
-        return this->Impl->Arguments;
-    }
+	std::vector<cmListFileArgument> const &Arguments() const noexcept
+	{
+		return this->Impl->Arguments;
+	}
 
-private:
-    struct Implementation
-    {
-        Implementation(std::string name, long line, long lineEnd,
-                       std::vector<cmListFileArgument> args)
-                : OriginalName{ std::move(name) }
-                , Line{ line }
-                , LineEnd{ lineEnd }
-                , Arguments{ std::move(args) }
-        {
-        }
+      private:
+	struct Implementation {
+		Implementation(std::string name, long line, long lineEnd,
+			       std::vector<cmListFileArgument> args)
+			: OriginalName{std::move(name)}, Line{line}, LineEnd{lineEnd},
+			  Arguments{std::move(args)}
+		{
+		}
 
-        std::string OriginalName;
-        long Line = 0;
-        long LineEnd = 0;
-        std::vector<cmListFileArgument> Arguments;
-    };
+		std::string OriginalName;
+		long Line = 0;
+		long LineEnd = 0;
+		std::vector<cmListFileArgument> Arguments;
+	};
 
-    std::shared_ptr<Implementation const> Impl;
+	std::shared_ptr<Implementation const> Impl;
 };
 
-struct cmListFileParser
-{
-    cmListFileParser();
-    ~cmListFileParser();
-    bool ParseFile(const char* filename);
-    bool Parse();
-    bool ParseFunction(const char* name, long line);
-    bool AddArgument(cmListFileLexer_Token* token,
-                     cmListFileArgument::Delimiter delim);
-    std::vector<cmListFileFunction> Functions;
-    const char* FileName = nullptr;
-    cmListFileLexer* Lexer;
-    std::string FunctionName;
-    long FunctionLine;
-    long FunctionLineEnd;
-    std::vector<cmListFileArgument> FunctionArguments;
-    enum
-    {
-        SeparationOkay,
-        SeparationWarning,
-        SeparationError
-    } Separation;
+struct cmListFileParser {
+	cmListFileParser();
+	~cmListFileParser();
+	bool ParseFile(const char *filename);
+	bool Parse();
+	bool ParseFunction(const char *name, long line);
+	bool AddArgument(cmListFileLexer_Token *token, cmListFileArgument::Delimiter delim);
+	std::vector<cmListFileFunction> Functions;
+	const char *FileName = nullptr;
+	cmListFileLexer *Lexer;
+	std::string FunctionName;
+	long FunctionLine;
+	long FunctionLineEnd;
+	std::vector<cmListFileArgument> FunctionArguments;
+	enum {
+		SeparationOkay,
+		SeparationWarning,
+		SeparationError
+	} Separation;
 };
